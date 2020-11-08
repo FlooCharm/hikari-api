532da56b58b94ab3e0b86bf44c33013eac547bc5
Jessica Sanchez <michelle.sr16@gmail.com>
2020-11-07 18:08:09 -0600

    added new fields to home model

--- models/home.js
+++ models/home.js
@@ -1,18 +1,57 @@
 const mongoose = require('mongoose');
 
 const HomeSchema = new mongoose.Schema({
-	temperature: {
-		type: Number
+	isManual: Boolean,
+	light: Number,
+	temperature: Number,
+	schedule: {
+		monday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		},
+		tuesday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		},
+		wednesday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		},
+		thursday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		},
+		friday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		},
+		saturday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		},
+		sunday: {
+			lightFrom: String,
+			lightTo: String,
+			blindsFrom: String,
+			blindsTo: String,
+		}
 	},
-	light: {
-		type: Number
-	},
-	isLightOn: {
-		type: Boolean
-	},
-	isManual: {
-		type: Boolean
-	}
+	lightIntensity: Number,
+	lightColor: String,
+	isFanOn: Boolean,
+	areBlindsOpen: Boolean
 })
 
 module.exports = mongoose.model('homes', HomeSchema);
