7b86dcf3e3657d571fb773d5d900eef6178e222c
Jessica Sanchez <michelle.sr16@gmail.com>
2020-11-21 19:40:23 -0600

    removed schedule from model

--- models/home.js
+++ models/home.js
@@ -4,50 +4,6 @@ const HomeSchema = new mongoose.Schema({
 	isManual: Boolean,
 	light: Number,
 	temperature: Number,
-	schedule: {
-		monday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		},
-		tuesday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		},
-		wednesday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		},
-		thursday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		},
-		friday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		},
-		saturday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		},
-		sunday: {
-			lightFrom: String,
-			lightTo: String,
-			blindsFrom: String,
-			blindsTo: String,
-		}
-	},
 	lightIntensity: Number,
 	lightColor: String,
 	isFanOn: Boolean,
